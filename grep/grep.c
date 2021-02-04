dfc957db671f7d5e19276fa1413b8b7ed7abd0fc
Gerlofs <root@gerlofs.me>
2021-02-04 16:52:07 +0000

    Clean up file structure, progress on variable line read

--- grep/grep.c
+++ grep/grep.c
@@ -7,14 +7,13 @@
  * 		1. Open file.
  * 		2. Pull a line and parse it for a pattern using strtok
  */
-
-struct args *parse_iargs(int argc, char **argv) {
-	if ( argc > 3 ) {
-		printf("Invalid number of parameters: %d, 3 required\n", argc);
+struct args *parse_iargs(int argc, char **argv, size_t nargs) {
+	if ( argc > (++nargs) ) {
+		printf("Invalid number of parameters: %d, %u required\n", argc, nargs);
 		exit(0);
 	}
 	
-	struct args *a = (struct args *) error_checked_malloc(sizeof(struct args));
+	struct args *a = (struct args *) error_checked_malloc(sizeof(struct args));	
 	
 	a->pattern = (char *) error_checked_malloc(strlen(argv[1]));
 	strncpy(a->pattern, argv[1], strlen(argv[1]));
@@ -24,47 +23,55 @@ struct args *parse_iargs(int argc, char **argv) {
 	a->filename[strlen(argv[2])] = '\0';
 
 	return a;
+
 }
 
 char *read_line(FILE *file_pointer) {
-	size_t buff_size = 128;
+	// TODO: Rewrite
+	
+	size_t buff_size = BUFF_INCR;
 	size_t pointer_pos = 0;
 	size_t line_len = 0;
-	char *line_pointer = (char *) error_checked_malloc(line_size);
-	char *write_pointer = p;
+	char *line_pointer = (char *) error_checked_malloc(BUFF_INCR);
+	char *buffer_pointer = (char *) error_checked_malloc(BUFF_INCR);
+	
 	for(;;) {
-		
-		if ((fgets(line_pointer, buff_size, file_pointer)) == NULL ) {
-			printf("DONE\n");
+		line_pointer = fgets(line_pointer, buff_size, file_pointer);
+
+		if (line_pointer == NULL) {
 			break;
 		}
 		
-		line_len = strlen(lp);
+		line_len = strlen(line_pointer);
+		strncpy(buffer_pointer+pointer_pos, line_pointer, line_len);
 		
-		if ( lp[line_len-1] == '\n') {
-			printf("DONE\n");
+		if ( line_pointer[line_len-1] == '\n') {
+			pointer_pos += line_len;
 			break;
 		}
 		
 		if (pointer_pos < LINE_LIMIT) {
-			lp = (char *) error_checked_realloc(lp, );
-			lp = lp + (line_size - line_size);
+			// TODO: Fix bug with reallocation (see: pointer_pos).
+			buffer_pointer = (char *) error_checked_realloc(buffer_pointer, buff_size+BUFF_INCR);
+			pointer_pos = buff_size-1;
+			buff_size += BUFF_INCR;
 		} else 
 		{
-			printf("%s, %u, %u\n", lp, line_size, line_len);
-			fprintf(stderr, "Error whilst parsing line into buffer of size %u.\n", line_size);
+			fprintf(stderr, "Error whilst parsing line into buffer of size %u.\n", buff_size);
 			exit(0);
 		}
 	}
 	
 	// I think fgets does this already, but just to be sure.
-	lp[line_size-1] = '\0';
-	return lp;
+	buffer_pointer[pointer_pos-1] = '\0';
+	free(line_pointer);
+	fprintf(stdout, "---- \n%s\n", buffer_pointer);
+	return buffer_pointer;
 }
 
 // TODO: Parse input arguments.
 int main(int argc, char **argv) {
-	struct args *a = parse_iargs(argc, argv);
+	struct args *a = parse_iargs(argc, argv, 2);
 	FILE *file_ptr = fopen(a->filename, "r");
 	if ( file_ptr == NULL ) {
 		fprintf(stderr, "Error opening file %s\n", a->filename);
@@ -72,7 +79,6 @@ int main(int argc, char **argv) {
 	}
 	
 	char *line = read_line(file_ptr);
-	fprintf(stdout, "%s\n", line);
 	fclose(file_ptr);
 	free(line);
 	free(a->filename);
--- grep/grep.h
+++ grep/grep.h
@@ -3,12 +3,13 @@
 #include <stdlib.h>
 #include <errno.h>
 #define LINE_LIMIT 2048 // Standard UNIX line limit.
+#define BUFF_INCR 128 	// Number of bytes to increase the buffer by each time we don't read a line end.
 
 struct args {
 	char *pattern;
 	char *filename;
 };
 
-struct args *parse_iargs(int, char**);
+struct args *parse_iargs(int, char**, size_t);
 char *read_line(FILE*);
 
