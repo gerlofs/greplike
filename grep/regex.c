54ac84c30700092930812cc101da2212c51ebd68
Gerlofs <root@gerlofs.me>
2021-02-12 15:32:19 +0000

    Add grouping function

--- grep/regex.c
+++ grep/regex.c
@@ -1,13 +1,10 @@
 #include "regex.h"
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
 // TODO:
 // 1. Implement + operator (similar to asterisk, seek all possible matches and work backwards to eliminate invalid character matches). [DONE].
 // 2. Implement grouping with ( and ) (unescaped). Might have to stop matching (or simply do this before starting to match), move the encapsulated expression into a new string and 
 //		run a seperate regex on it using the subsequent operator (if there is one, if there's not we just ignore the parentheses).
-// 3. Implement optional operator (?): Ignore the character entirely and continue the match on line_text+1 with regex+2. [DONE
+// 3. Implement optional operator (?): Ignore the character entirely and continue the match on line_text+1 with regex+2. [DONE].
 // 4. Implement tests.
 
 int regex_find(char *regular_expression, char *line_text) {
@@ -35,6 +32,13 @@ int regex_match(char *regular_expression, char *line_text) {
 	if ( current_char == 0x24 && next_char == 0x00 ) return (*line_text == 0x00);
 	// If we've exhausted the regex pattern, we've matched.
 	else if ( current_char == 0x00 ) return 1;
+	// If we have an un-escaped bracket, we're grouping.
+	else if ( ( current_char != 0x5C && next_char == 0x28 ) || current_char == 0x28 ) { 
+		unsigned offset = ( current_char == 0x28 ) ? 1 : 2;
+		char *group = create_group(regular_expression+offset);
+		printf("Grouped: %s\n", group);
+		exit(1);
+	}
 	// If next character is an optional, either the next character matches the character after the optional OR the current one does.
 	else if ( next_char == 0x3F ) return (regex_match(regular_expression+2, line_text) || regex_match(regular_expression+2, line_text+1));
 	// If there's a 'one or more' or a 'none or more' operator we start a different expression check (see func).
@@ -83,6 +87,33 @@ int multi_match_single_char(unsigned match_n, char to_match, char *regular_expre
 	return 0;
 }
 
+char *create_group(char *regex_ptr) {
+	/*	Seek until we find a closing parenthesis, 
+	*	create a new string of size equal to the number of characters between the parentheses,
+	*	fill the new buffer by copying across part of the original regular expression,
+	*	TODO: Do we have to remove the grouping from the original regular expression? (E.g. what happens if we fail a grouping check, does the regex engine then try to create a group again?).
+	*/
+	size_t group_size;
+	char *read_ptr = regex_ptr;
+
+	// Parse the string until we reach the end parentheses or the EOL.
+	for ( group_size = 0; *read_ptr != 0x00 && *read_ptr != 0x29; group_size++,read_ptr++);
+	
+	if ( *read_ptr == 0x00 ) {
+		fprintf(stderr, "Invalid regular expression given: no closing parentheses in grouping [%s]", regular_expression);
+		exit(0);
+	}
+	
+	char *group = (char *) error_checked_malloc(group_size+1);
+	if ( strlen(strncpy(group, regex_ptr, group_size)) < 0 ) {
+		fprintf(stderr, "Unable to copy string %s [offset +%u] to a new pointer!", regex_ptr, group_size);
+		exit(0);
+	}
+	
+	group[group_size] = (char) 0x00;
+	return group;
+}
+
 int group_and_match() {
 	// Seek through the expression to find the end of the group (right parentheses), if we don't find it then the expression is invalid (we should add a check for this at the beginning
 	//	of the program though). 
@@ -90,4 +121,10 @@ int group_and_match() {
 	//	for each character in the regex.
 	// We may need to alter multi_match to take a character pointer for to_match to allow for grouping, it should check strlen == 1 and work like normal if a string isn't given.
 	return 0;
+}
+
+int main(void) {
+	char *reg = "(abc)+d";
+	char *txt = "cdbabcfabcddefg";
+	printf("%d\n", regex_find(reg, txt));
 }
--- grep/regex.h
+++ grep/regex.h
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "../alloc.h"
+
 int regex_find(char*, char*);
 int regex_match(char*, char*);
 int multi_match_single_char(unsigned, char, char*, char*);
+char *create_group(char*);
