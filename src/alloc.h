0bc3641dc5df8164c6188144fd3fa1ab3c577dc2
Gerlofs <root@gerlofs.me>
2021-02-22 17:01:31 +0000

    Move to src

---
+++ src/alloc.h
@@ -0,0 +1,28 @@
+#ifndef ALLOC_H
+#define ALLOC_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void *error_checked_malloc(size_t);
+void *error_checked_realloc(void*, size_t);
+
+void *error_checked_malloc(size_t nbytes) {
+	void *v_ptr = malloc(nbytes);
+	if ( v_ptr == NULL ) {
+		fprintf(stderr, "Error whilst allocating %u bytes\n", nbytes);
+		exit(0);
+	}
+	return v_ptr;
+}
+
+void *error_checked_realloc(void *ptr, size_t nbytes) {
+	ptr = realloc(ptr, nbytes);
+	if ( ptr == NULL ) {
+		fprintf(stderr, "Error whilst reallocating %u bytes\n", nbytes);
+		exit(0);
+	}
+	return ptr;
+}
+
+#endif /* ALLOC_H */
